d4cd6979fd88d309c191aa3929a9b021f4eda876
AramArutyunov <zobyhh@gmail.com>
2024-04-12 03:12:21 +0300

    ТГ бот

--- app.py
+++ app.py
@@ -5,7 +5,16 @@ from extensions import APIException, CurrencyConverter
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
+
+@bot.message_handler(commands=['start'])
+def start(message: telebot.types.Message):
+    text = ('Это чат бот для конвертации валюты \nчтобы начать работу введите команду боту в следующем формате:'
+'\n<имя валюты>  <в какую валюту перевести> <количество переводимой валюты>\nУвидеть список доступных валют: /values'
+'\nпомощь: /help')
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['help'])
 def help(message: telebot.types.Message):
     text = 'чтобы начать работу введите команду боту в следующем формате: \n<имя валюты> \
 <в какую валюту перевести> \
